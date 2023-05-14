1140aa349c96ce2d7b5ef1ce3dfd03a9ed3190e3
m3tler <dawid.metlerski@outlook.com>
2023-05-14 13:56:52 +0200

    Init commit

---
+++ src/main/java/com/example/pizzarestaurantapi/PizzaRestaurantApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzarestaurantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaRestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaRestaurantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pizzarestaurantapi/PizzaRestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzarestaurantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaRestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
